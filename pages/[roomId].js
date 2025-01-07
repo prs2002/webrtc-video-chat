4fbab1c153978a7e0fb6c7a829e94ebd02a93a72
prs2002 <praneetsuyal+github@gmail.com>
2025-01-07 13:11:02 +0530

    stream setup

--- pages/[roomId].js
+++ pages/[roomId].js
@@ -1,8 +1,14 @@
 import { useEffect } from "react";
 import { useSocket } from "@/context/socket";
 import usePeer from "@/hooks/usePeer";
+import useMediaStream from "@/hooks/useMediaStream";
+import Player from "@/component/Player";
+
 const Room = () => {
   const socket = useSocket();
   const { peer, myId } = usePeer();
+  const {stream} = useMediaStream()
+  return (<div>
+    <Player url={stream} muted playing playerId={myId}/>
+  </div>)
 };
-export default Room;
